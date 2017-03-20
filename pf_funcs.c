6500a44c0cb2023992751fd1f4eb0f7f62b8ee64
KRLoyd <135@holbertonschool.com>
2017-03-20 23:02:07 +0000

    correct functions

--- pf_funcs.c
+++ pf_funcs.c
@@ -1,38 +1,54 @@
 #include "holberton.h"
 
 /**
- ** print_char - prints a character
- ** @copy: list of arguments passed
+ ** copy_char - copies a character to buffer
+ ** @print_list: list of arguments passed
+ ** @buffer: pointer to buffer to copy to
+ ** @index: specific index in buffer to copy to
+ ** Return: index of newly copied character
  **/
-void print_char(va_list copy)
+int copy_char(va_list print_list, char *buffer, int index)
 {
 	char x;
-	x = va_arg(copy, int);
-	_putchar(x);
+	x = va_arg(print_list, int);
+	buffer[index] = x;
+	return (index);
 }
 
 /**
- ** print_string - prints a string
- ** @copy: list of arguments passed
+ ** copy_string - copies a string to buffer
+ ** @print_list: list of arguments passed
+ ** @buffer: buffer to safe to
+ ** @index: index in buffer to copy to
+ ** Return: index of newly copied string
  **/
-void print_string(va_list copy)
+int copy_string(va_list print_list, char *buffer, int index)
 {
+	int i;
 	char *x;
-	x = va_arg(copy, char *);
-	_puts(x);
+	x = va_arg(print_list, char *);
+	for (i = 0; x[i] != '\0'; i++, index++)
+	{
+		buffer[index] = x[i];
+	}
+	return (index - 1);
 }
 
 /**
- ** print_mod - prints the modulo character
- ** @copy: list of arguments passed
+ ** copy_mod - copies the modulo character to buffer
+ ** @print_list: list of arguments passed
+ ** @buffer: buffer to copy to
+ ** @index: index of buffer to copy to
+ ** Return: indexx of newly copied modulo
  **/
-void print_char()
+int copy_mod(va_list print_list, char *buffer, int index)
 {
-	_putchar('%');
-	_putchar('\n');
+	(void)print_list;
+	buffer[index] = '%';
+	return (index);
 }
 
-/* /**
+/**
  ** print_string - prints a string
  ** @copy: list of arguments passed
  **/
