ecb42184596721848e00ab0d111aacc27251b56e
KRLoyd <135@holbertonschool.com>
2017-03-20 17:02:38 +0000

    update header file

--- holberton.h
+++ holberton.h
@@ -12,8 +12,10 @@ typedef struct find_pf_func
 	char *input;
 	void (*f) ();
 }find_pf_func;
-void print_char (va_list copy);
-void print_string (va_list copy);
+void (*get_pf_func(char *s))();
+void print_char(va_list copy);
+void print_string(va_list copy);
+void print_mod();
 int _putchar(char c);
 void _puts(char *str);
 #endif /* HOLBERTON_H */
