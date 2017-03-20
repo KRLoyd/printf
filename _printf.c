246c8fb5ef9aab15d375c781aca8130155097278
KRLoyd <135@holbertonschool.com>
2017-03-20 23:01:31 +0000

    fix code to work with a buffer

--- _printf.c
+++ _printf.c
@@ -6,33 +6,88 @@
  **
  ** Description: prints each argument to standard output per the specified
  ** type of input
- ** Return: 0 Success, FAILURE???
+ ** Return: 0 Success
  **/
 
 int _printf(const char *format, ...)
 {
-	char *copy;
-	unsigned int i, j;
-
-	find_pf_func pf_func = {
-		{"c", print_char},
-		{"s", print_string},
-		{NULL, NULL}
+	char buffer[1024];
+	unsigned int i, j, index;
+	va_list print_list;
+	find_pf_func pf_func[] = {
+		{'c', copy_char},
+		{'s', copy_string},
+		{'%', copy_mod},
+		{'\0', NULL}
 	};
 
-	va_list print_list;
-	va_copy(printlist, copy);
-	va_start(copy, format);
+/* initialize print_list */
+	va_start(print_list, format);
 
+	index = 0;
 	i = 0;
+	clear_buff(buffer);
 
-	while (copy != NULL && copy[i] != '\0')
+/* Print chars before first % and copy chars after % to copy */
+	while (format && format[i] != '\0')
 	{
-		j = 0;
-		get_pf_func(copy[i]);
+		if (format[i] != '%')
+		{
+			buffer[index] = format[i];
+/*			index++;
+			i++; */
+		}
+		else
+		{
+			i++;
+			j = 0;
+			while (pf_func[j].input != '\0')
+			{
+				if (pf_func[j].input == format[i])
+				{
+					index = pf_func[j].f(print_list, buffer, index);
+				}
+				j++;
+			}
+		}
 		i++;
+		index++;
 	}
-
 	va_end(print_list);
-	Return (0);
+	_strprint(buffer);
+	clear_buff(buffer);
+	return (index);
+}
+
+
+/**
+ ** _strprint - print a string
+ ** @str: pointer to string to print
+ ** Return: void
+ **/
+void _strprint(char *str)
+{
+	int i;
+	if (str != '\0')
+	{
+		while (str[i] != '\0')
+		{
+			_putchar(str[i]);
+			i++;
+		}
+	}
+}
+
+/**
+ ** clear_buff - sets buffer to 0
+ ** @buffer: pointer to memory of buffer
+ ** Return: void
+ **/
+void clear_buff(char *buffer)
+{
+	int i;
+	for (i = 0; i < 1024; i++)
+	{
+		buffer[i] = 0;
+	}
 }
--- get_pf_func.c
+++ get_pf_func.c
@@ -1,26 +1,30 @@
 #include "holberton.h"
 
-/** find_pf_func - get correct _printf function
+/** _pf_func - get correct _printf function
  ** @s: conversion specifier passed
  **
  **/
 
-void (*get_pf_func(char *s))()
+void (*get_pf_func(char s))(va_list)
 {
 
-	find_pf_func pf_func = {
-		{"c", print_char},
-		{"s", print_string},
-		{"%", print_mod},
+	find_pf_func pf_func[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'%', print_mod},
 		{NULL, NULL}
 	};
-	unsigned int i;
+	unsigned int i, j;
+
+	i = 0;
+	j = 0;
 
 	while (pf_func[i].input != NULL)
 	{
 		if (s == pf_func[i].input)
 		{
-			return (pf_func[i]).f(print_list);
+			return (pf_func[i].f);
+			j++;
 		}
 		i++;
 	}
