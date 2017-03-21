5da8055db8e12b974d3c7825796b834ffa25b9cd
KRLoyd <135@holbertonschool.com>
2017-03-21 17:37:00 +0000

    rm proto for get_pf_func

--- holberton.h
+++ holberton.h
@@ -6,16 +6,17 @@
 #include <stdlib.h>
 int _printf(const char *format, ...);
 /**
-** find_pf_func - find the correct printf fucntion to use
+** struct find_pf_func - struct
 ** @input: input to determine type of printf function
 ** @f: specific printf function
+**
+** Description: the the correct copy function to use
 **/
 typedef struct find_pf_func
 {
 	char input;
 	int (*f)();
-}find_pf_func;
-void (*get_pf_func)(va_list print_list, char *buffer, int index);
+} find_pf_func;
 int copy_char(va_list print_list, char *buffer, int index);
 int copy_string(va_list print_list, char *buffer, int index);
 int copy_mod(va_list print_list, char *buffer, int index);
@@ -25,4 +26,5 @@ int _strlen(char *);
 char *_strcpy(char *dest, char *src);
 void _strprint(char *str);
 void clear_buff(char *buffer);
+int put_null(char *buffer, int index);
 #endif /* HOLBERTON_H */
