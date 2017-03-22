8e576b1f8d7ea23310ec27364afc5145ea8587d3
KRLoyd <135@holbertonschool.com>
2017-03-22 02:49:01 +0000

    remove unsigned int code

--- pf_funcs.c
+++ pf_funcs.c
@@ -64,50 +64,6 @@ int copy_mod(va_list print_list, char *buffer, int index)
 	buffer[index] = '%';
 	return (index);
 }
-/**
- ** copy_unsigned_int - copies an int to buffer
- ** @print_list: list of arguments passed
- ** @buffer: buffer to copy to
- ** @index: index of buffer to copy to
- ** Return: index of newly copied int
- **/
-/**
- * int copy_unsigned_int(va_list print_list, char *buffer, int index)
- *{
- *	int  b, div, len, last;
- *	unsigned long int n;
- *
- *	div = 1;
- *	len = 0;
- *	n = va_arg(print_list, int);
- *	last = n % 10;
- *
- *	n = n / 10;
- *
- *	/* find divisor
- *	b = n;
- *
- *	while (b > 0)
- *	{
- *	len += 1;
- *	div *= 10;
- *	b /= 10;
- *	}
- *	div /= 10;
- *
- * print the digits
- *	while (len >= 1)
- *	{
- *		buffer[index] = n / div + '0';
- *		n = n % div;
- *		div = div / 10;
- *		len--;
- *		index++;
- *	}
- *	buffer[index] = last + '0';
- *	return (index);
- *	}
-*/
 
 /**
  ** copy_int - copies an int to buffer
